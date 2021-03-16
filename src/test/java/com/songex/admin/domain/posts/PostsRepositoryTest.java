4c8876911dc599a6b249046ec6471454d4200c10
car0628@naver.com <thddud!2>
2021-03-16 11:03:15 +0900

    Posts 등록까지 테스트 완료 + 주석 추가

--- src/test/java/com/songex/admin/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/songex/admin/domain/posts/PostsRepositoryTest.java
@@ -20,7 +20,7 @@ public class PostsRepositoryTest {
     @Autowired
     PostsRepository postsRepository;
 
-    @After //
+    @After // 단위 테스트가 끝날 때마다 수행되는 메소드를 지정, 테스트간 데이터 침범을 막기 위해 사용, 여러 테스트가 동시에 수행되면 테스트용 DB인 H2에 데이터가 그대로 남아 있어 다음 테스트 실행 시 테스트가 실패할 수 있음
     public void cleanup() {
         postsRepository.deleteAll();
     }
@@ -31,14 +31,14 @@ public class PostsRepositoryTest {
         String title = "테스트 게시글";
         String content = "테스트 본문";
 
-        postsRepository.save(Posts.builder() //
+        postsRepository.save(Posts.builder() // 테이블 posts에 insert/update 쿼리를 실행, id값이 있다면 update, 없다면 insert 쿼리 실행
                 .title(title)
                 .content(content)
                 .author("car0628@naver.com")
                 .build());
 
         //when
-        List<Posts> postsList = postsRepository.findAll(); //
+        List<Posts> postsList = postsRepository.findAll(); // 테이블 posts에 있는 모든 데이터를 조회해오는 메소드
 
         //then
         Posts posts = postsList.get(0);
