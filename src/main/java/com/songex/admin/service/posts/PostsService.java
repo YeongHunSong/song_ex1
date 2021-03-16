2d273e1ed28195dce1d98b3e9743e88ceeb2b1f7
car0628@naver.com <thddud!2>
2021-03-16 10:47:08 +0900

    Posts 등록까지 테스트 완료

---
+++ src/main/java/com/songex/admin/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.songex.admin.service.posts;
+
+import com.songex.admin.domain.posts.PostsRepository;
+import com.songex.admin.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
