749b53dbc606fc224fedab774097c5bd94a8ce7a
car0628@naver.com <thddud!2>
2021-03-17 23:02:40 +0900

    게시판 CRUD 구현 완성

--- src/main/java/com/songex/admin/domain/posts/PostsRepository.java
+++ src/main/java/com/songex/admin/domain/posts/PostsRepository.java
@@ -2,6 +2,13 @@ package com.songex.admin.domain.posts;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC") // SpringDataJPA에서 제공하지 않는 메소드는 직접 쿼리 작성해도 가능 @Query를 이용해서, 실제로는 JPA로도 가능 하긴함
+    // 큰 규모의 프로젝트에서는 데이터 조회의 경우 querydsl(IDE에서 오타 검출 가능해 추천), jooq, MyBatis 같은 프레임워크를 사용하고 등록/수정/삭제 등은 SpringDataJPA를 통해 진행하는 경우가 많음
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/songex/admin/service/posts/PostsService.java
+++ src/main/java/com/songex/admin/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.songex.admin.service.posts;
 
 import com.songex.admin.domain.posts.Posts;
 import com.songex.admin.domain.posts.PostsRepository;
+import com.songex.admin.web.dto.PostsListResponseDto;
 import com.songex.admin.web.dto.PostsResponseDto;
 import com.songex.admin.web.dto.PostsSaveRequestDto;
 import com.songex.admin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,17 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new
                 IllegalArgumentException("해당 게시글이 없습니다 ID = " + id));
                 return  new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하되 조회 기능만 남겨두어 조회속도가 개선됨 (등록, 수정, 삭제 기능이 없는 서비스 메소드에서 추천)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+        // .map(PostsListResponseDto::new) = .map(posts -> new PostsListResponseDto(posts))
+    }
+    
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다 ID = " + id));
+        postsRepository.delete(posts); //JAPRepository에서 delete 메소드를 지원하기 때문에 활용, 엔티티를 파라미터로 삭제할 수도 있고 deleteById 메소드를 이용하면 id로 삭제할 수도 있음, 존재하는 Posts인지 확인을 하기 위해 엔티티 조회 후 그대로 삭제
     }
 }
