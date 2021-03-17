f7f17227cb694c220e1795da581cc5b799599edb
car0628@naver.com <thddud!2>
2021-03-17 09:10:17 +0900

    Update 완료

--- src/main/java/com/songex/admin/service/posts/PostsService.java
+++ src/main/java/com/songex/admin/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.songex.admin.service.posts;
 
+import com.songex.admin.domain.posts.Posts;
 import com.songex.admin.domain.posts.PostsRepository;
+import com.songex.admin.web.dto.PostsResponseDto;
 import com.songex.admin.web.dto.PostsSaveRequestDto;
+import com.songex.admin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,19 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다 ID = " + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+                return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다 ID = " + id));
+                return  new PostsResponseDto(entity);
     }
 }
