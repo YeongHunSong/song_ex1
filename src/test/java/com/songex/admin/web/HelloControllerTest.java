e17f48571a1fcef54f613fd369c37cf4d2833339
car0628@naver.com <thddud!2>
2021-03-23 01:14:50 +0900

    기타 자잘한 수정

--- src/test/java/com/songex/admin/web/HelloControllerTest.java
+++ src/test/java/com/songex/admin/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.songex.admin.web;
 
+import com.songex.admin.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,12 +16,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) // 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킴 스프링 부트 테스트와 JUnit사이에 연결자 역할
-@WebMvcTest(controllers = HelloController.class) // Spring MVC에 집중할 수 있는 어노테이션으로 @Controller, @ControllerAdvice 등을 사용할 수 있음
+@WebMvcTest(controllers = HelloController.class, // Spring MVC에 집중할 수 있는 어노테이션으로 @Controller, @ControllerAdvice 등을 사용할 수 있음
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired // 스프링이 관리하는 빈을 주입 받음
     private MockMvc mvc; // 웹 API를 테스트할 때 사용 스프링 MVC 테스트의 시작점
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -27,6 +36,7 @@ public class HelloControllerTest {
             .andExpect(content().string(hello)); // mvc.preform의 결과를 검증 / 응답 본문의 내용을 검증 hello를 리턴하는지, 이 값이 맞는지 검증
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
