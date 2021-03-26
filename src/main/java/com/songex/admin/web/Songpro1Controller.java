9c1ce81dfffaf81da58ade10bd05120bfbbb0b7b
car0628@naver.com <thddud!2>
2021-03-26 21:03:42 +0900

    프론트엔드

--- src/main/java/com/songex/admin/web/Songpro1Controller.java
+++ src/main/java/com/songex/admin/web/Songpro1Controller.java
@@ -13,9 +13,19 @@ public class Songpro1Controller {
         return "songpro1/mainpage";
     }
 
+    @GetMapping( "/mainpage-login")
+    public String mainPage_Login() {
+        return "songpro1/mainpage-login";
+    }
+
     @GetMapping("/signup")
     public String signUp() {
         return "songpro1/signup";
+    }
+
+    @GetMapping( "/memberdata")
+    public String memberData() {
+        return "songpro1/memberdata";
     }
 
     @GetMapping( "/loginn")
@@ -23,13 +33,38 @@ public class Songpro1Controller {
         return "songpro1/loginn";
     }
 
-    @GetMapping( "/mainpage-login")
-    public String mainPage_Login() {
-        return "songpro1/mainpage-login";
+    @GetMapping( "/category")
+    public String category() {
+        return "songpro1/category";
     }
 
-    @GetMapping( "/memberdata")
-    public String memberData() {
-        return "songpro1/memberdata";
+    @GetMapping( "/myreply")
+    public String myReply() {
+        return "songpro1/myreply";
+    }
+
+    @GetMapping( "/mytext")
+    public String myText() {
+        return "songpro1/mytext";
+    }
+
+    @GetMapping( "/postdetail")
+    public String postDetail() {
+        return "songpro1/postdetail";
+    }
+
+    @GetMapping( "/searchid")
+    public String searchId() {
+        return "songpro1/search-id";
+    }
+
+    @GetMapping( "/searchpw")
+    public String searchPw() {
+        return "songpro1/search-pw";
+    }
+
+    @GetMapping( "/searchresult")
+    public String searchResult() {
+        return "songpro1/search-result";
     }
 }
