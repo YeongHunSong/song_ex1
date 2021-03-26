c72a4a40ed41017c4cb52b33fb03397f95aaacd9
car0628@naver.com <thddud!2>
2021-03-26 18:25:30 +0900

    프론트엔드 뜨게 해보기

---
+++ src/main/java/com/songex/admin/web/Songpro1Controller.java
@@ -0,0 +1,13 @@
+package com.songex.admin.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class Songpro1Controller {
+
+    @GetMapping("/songpro1/mainpage")
+    public String songpro1() {return "songpro1/mainpage"; }
+}
