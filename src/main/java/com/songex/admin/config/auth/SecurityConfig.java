a40e191282a64100c592ad73a91e0727bf453855
car0628@naver.com <thddud!2>
2021-03-28 23:51:35 +0900

    무중단 배포 환경 설정

--- src/main/java/com/songex/admin/config/auth/SecurityConfig.java
+++ src/main/java/com/songex/admin/config/auth/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable().headers().frameOptions().disable() // h2-console 화면을 사용하기 위해 해당 옵션들을 disable 함
                 /*.and()
                     .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점, authorizeRequests가 선언되어야만 antMatchers 옵션을 사용할 수 있음
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll() // "/"등 지정된 URL들은 permitAll()옵션을 통해 전체 열람 권한을 주었음
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll() // "/"등 지정된 URL들은 permitAll()옵션을 통해 전체 열람 권한을 주었음, /profile이 인증 없이도 호출될 수 있게 제외 코드 추가
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // antMatchers -> 권한 관리 대상을 지정하는 옵션 URL, HTTP 메소드별로 관리가 가능, "/api/v1/**" 주소를 가진 API는 USER 권한을 가진 사람만 가능하도록 설정
                     .anyRequest().authenticated() //설정된 값들 이외 나머지 URL들을 나타냄, 여기서는 authenticated()을 추가하여 나머지 URL들은 모두 인증된 사용자들에게만 허용, 인증된 사용자 즉 로그인한 사용자들을 말함*/
                 .and()
